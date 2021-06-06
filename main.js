efe4b7c7b49def45f337a9f5df091b08056611f9
makenasandra <makenawachira@gmail.com>
2021-06-06 12:52:51 +0300

    added error handling capabilty

--- main.js
+++ main.js
@@ -1,31 +1,61 @@
-let express = require('express')
-let bodyParser = require('body-parser')
-let app = express() 
-let http = require('http').Server(app)
-let  io = require('socket.io')(http)
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express() 
+const http = require('http').Server(app)
+const  io = require('socket.io')(http)
+const mongoose =  require("mongoose")
+let dbURL = require('./config.js');
+
 
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name: 'Makena', message: 'Hi'},
-    {name: 'Opiyo', message: 'Hello'}
-]
+mongoose.Promise = Promise
 
-app.get('/messages', (req, res) =>{
-    res.send(messages)
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
-app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+    })
+})
+
+app.post('/messages', async (req, res) => {
+
+    try {
+        //throw 'some error'
+        var message = new Message(req.body)
+
+        var savedMessage = await message.save()
+
+        console.log('saved')
+
+        var censored = await Message.findOne({ message: 'badword' })
+
+        if (censored)
+            await Message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body)
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    }
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbURL, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
